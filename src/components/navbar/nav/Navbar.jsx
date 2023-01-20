7e8f08c5955bbcab797a50a2fbe986356951a19c
nahuel cempellin <nahuelcempellin@gmail.com>
2023-01-20 17:13:45 -0300

    about popup

--- src/components/navbar/nav/Navbar.jsx
+++ src/components/navbar/nav/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import{BsFillTriangleFill,BsFillJournalBookmarkFill} from 'react-icons/bs'
 import { NavLink } from 'react-router-dom'
 import Logo from '../../logo/Logo'
 import './Navbar.css'
@@ -11,7 +12,20 @@ const Navbar = () => {
         </div>
 
         <div className='menu-box'>
+            <div className='box-about-cont'>
             <NavLink>About</NavLink>
+            <BsFillTriangleFill className='triangle'/>
+            <div className='box-about'>
+              <p>About me</p>
+              <div className='menu-about-container'>
+               <a>
+                 <BsFillJournalBookmarkFill/>
+                 Studies
+              </a>
+              </div>
+              
+            </div>
+            </div>
             <NavLink>Projects</NavLink>
             <a href='#Footer' className='contacto'>Contact</a>
             <NavLink>Cofee</NavLink>
