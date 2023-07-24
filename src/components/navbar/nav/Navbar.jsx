309d81db76c9b461ee38ce8245eca6746ffc7660
Nahuel Cempellin <nahuelcempellin@gmail.com>
2023-07-24 15:36:40 -0300

    sacando signin

--- src/components/navbar/nav/Navbar.jsx
+++ src/components/navbar/nav/Navbar.jsx
@@ -1,70 +1,70 @@
-import React from 'react'
-import{BsFillTriangleFill,BsFillJournalBookmarkFill} from 'react-icons/bs'
-import{AiOutlineLink} from 'react-icons/ai'
-import {CgProfile} from 'react-icons/cg'
-import {CiSettings} from 'react-icons/ci'
-import { NavLink } from 'react-router-dom'
-import Logo from '../../logo/Logo'
-import './Navbar.css'
+import React from "react";
+import { BsFillTriangleFill, BsFillJournalBookmarkFill } from "react-icons/bs";
+import { AiOutlineLink } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { CiSettings } from "react-icons/ci";
+import { NavLink } from "react-router-dom";
+import Logo from "../../logo/Logo";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='NavContainer'>
-        <div>
-            <Logo/>
-        </div>
+    <div className="NavContainer">
+      <div>
+        <Logo />
+      </div>
 
-        <div className='menu-box'>
-            <div className='box-about-cont'>
-            <NavLink to={'/'}>About</NavLink>
-            <BsFillTriangleFill className='triangle'/>
-            <div className='box-about'>
-              <p>About...</p>
-              <div className='menu-about-container'>
-              
+      <div className="menu-box">
+        <div className="box-about-cont">
+          <NavLink to={"/"}>About</NavLink>
+          <BsFillTriangleFill className="triangle" />
+          <div className="box-about">
+            <p>About...</p>
+            <div className="menu-about-container">
               <a>
-                <div className='circle-about-item'>
-                 <CgProfile className='item-color'/>
+                <div className="circle-about-item">
+                  <CgProfile className="item-color" />
                 </div>
-                 Me
+                Me
               </a>
 
-               <a href='#studies'>
-                <div className='circle-about-item'>
-                 <BsFillJournalBookmarkFill className='item-color'/>
+              <a href="#studies">
+                <div className="circle-about-item">
+                  <BsFillJournalBookmarkFill className="item-color" />
                 </div>
-                 Studies
+                Studies
               </a>
 
-              <a href='#project'>
-                <div className='circle-about-item'>
-                 <CiSettings className='item-color'/>
+              <a href="#project">
+                <div className="circle-about-item">
+                  <CiSettings className="item-color" />
                 </div>
-                 Work experience
+                Work experience
               </a>
 
               <a>
-                <div className='circle-about-item'>
-                 <AiOutlineLink className='item-color'/>
+                <div className="circle-about-item">
+                  <AiOutlineLink className="item-color" />
                 </div>
-                 Links
+                Links
               </a>
-              </div>          
             </div>
-            </div>
-
-            <NavLink to={'/career'}>Career</NavLink>
-
-            <NavLink to={'/projects'}>Projects</NavLink>
-            <a href='#Footer' className='contacto'>Contact</a>
-            
+          </div>
         </div>
 
-        <div className='signin'>
-            <NavLink to={'/signin'}>Sign In</NavLink>
-        </div>
+        <NavLink to={"/career"}>Career</NavLink>
+
+        <NavLink to={"/projects"}>Projects</NavLink>
+        <a href="#Footer" className="contacto">
+          Contact
+        </a>
+      </div>
+
+      <div className="signin">
+        {/* <NavLink to={"/signin"}>Sign In</NavLink> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/navbar/navresponsive/NavResponsive.jsx
+++ src/components/navbar/navresponsive/NavResponsive.jsx
@@ -1,66 +1,84 @@
-import React,{useState} from 'react'
-import Logo from '../../logo/Logo'
-import {RxHamburgerMenu} from 'react-icons/rx'
-import {BsArrowLeft} from 'react-icons/bs'
-import{BsFillJournalBookmarkFill} from 'react-icons/bs'
-import{BiCoffeeTogo} from 'react-icons/bi'
-import {MdContactSupport} from 'react-icons/md'
-import {CiSettings} from 'react-icons/ci'
-import '../nav/Navbar.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Logo from "../../logo/Logo";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { BsArrowLeft } from "react-icons/bs";
+import { BsFillJournalBookmarkFill } from "react-icons/bs";
+import { BiCoffeeTogo } from "react-icons/bi";
+import { MdContactSupport } from "react-icons/md";
+import { CiSettings } from "react-icons/ci";
+import "../nav/Navbar.css";
+import { Link } from "react-router-dom";
 
 const NavResponsive = () => {
-  const[popOn, setPopOn]=useState(false)
-
-  
+  const [popOn, setPopOn] = useState(false);
 
   return (
-    <div className='NavContainer-responsive'>
-        <Logo/>
-        <button className='burguer-button' onClick={()=>setPopOn(!popOn)}>
-        <RxHamburgerMenu className='burguer'/>
+    <div className="NavContainer-responsive">
+      <Logo />
+      <button className="burguer-button" onClick={() => setPopOn(!popOn)}>
+        <RxHamburgerMenu className="burguer" />
+      </button>
+
+      <div className={popOn === true ? "popup-burguer-menu" : "pop-off"}>
+        <button className="close" onClick={() => setPopOn(false)}>
+          <BsArrowLeft className="close" />
         </button>
 
-        <div className={popOn === true? 'popup-burguer-menu' : 'pop-off'}>
-            <button className='close' onClick={()=>setPopOn(false)}><BsArrowLeft className='close'/></button>
+        <div className="menu-items-responsive">
+          <Link
+            to={"/"}
+            className="item-container"
+            onClick={() => setPopOn(false)}
+          >
+            <div id="circle">
+              <BsFillJournalBookmarkFill />
+            </div>
+            <p>About</p>
+          </Link>
 
-        <div className='menu-items-responsive'>
+          <Link
+            to={"/career"}
+            className="item-container"
+            onClick={() => setPopOn(false)}
+          >
+            <div id="circle">
+              <BiCoffeeTogo />
+            </div>
+            <p>Career</p>
+          </Link>
 
-        <Link to={'/'} className='item-container' onClick={()=>setPopOn(false)}>
-          <div id='circle'><BsFillJournalBookmarkFill/></div>
-          <p>About</p>
-        </Link>
+          <Link
+            to={"/projects"}
+            className="item-container"
+            onClick={() => setPopOn(false)}
+          >
+            <div id="circle">
+              <CiSettings />
+            </div>
+            <p>Projects</p>
+          </Link>
 
-          <Link to={'/career'} className='item-container' onClick={()=>setPopOn(false)}>
-          <div id='circle'><BiCoffeeTogo/></div>
-          <p>Career</p>
-        </Link>
-
-
-        <Link to={'/projects'} className='item-container' onClick={()=>setPopOn(false)}>
-          <div id='circle'><CiSettings/></div>
-          <p>Projects</p>
-        </Link>
-
-
-        <a href='#Footer'className='item-container' onClick={()=>setPopOn(false)}>
-          <div id='circle'><MdContactSupport/></div>
-          <p>Contact</p>
-        </a>
-
-
-      
+          <a
+            href="#Footer"
+            className="item-container"
+            onClick={() => setPopOn(false)}
+          >
+            <div id="circle">
+              <MdContactSupport />
+            </div>
+            <p>Contact</p>
+          </a>
         </div>
 
-        <div className='final-menu'>
-          <a href='#Footer' onClick={()=>setPopOn(false)}>Contact me</a>
-          <Link to={'/signin'} onClick={()=>setPopOn(false)}>Sign in</Link>
+        <div className="final-menu">
+          <a href="#Footer" onClick={() => setPopOn(false)}>
+            Contact me
+          </a>
+          {/* <Link to={'/signin'} onClick={()=>setPopOn(false)}>Sign in</Link> */}
         </div>
-
-        
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavResponsive
+export default NavResponsive;
